b9296af0c34ecb1acbea2536020cf59822f824b9
Horacio Gonzalez <horacio.gonzalez@gmail.com>
2021-01-25 02:30:16 +0100

    Step 05 solutions

---
+++ step-05/stencil-beers/src/components/beer-list/beer-list.tsx
@@ -0,0 +1,110 @@
+import { Component, Prop, State, h } from '@stencil/core';
+import { beerList, Beer, sortingCriteria, sortingCriterion } from '../../utils/beers';
+
+@Component({
+  tag: 'beer-list',
+  styleUrl: 'beer-list.css',
+})
+export class BeerList {
+
+    @State() beers: Array<Beer>;
+    @State() pattern: string;
+    @State() criterion: sortingCriterion;
+    @State() descendingSort: boolean;
+
+    componentWillLoad() {
+        this.beers = beerList;
+        this.criterion = sortingCriteria[0];
+        this.descendingSort = false;
+    }
+
+    doFilter(evt: Event) {
+        this.pattern = (evt.target as HTMLInputElement).value;
+    } 
+
+    _currentBeers(): number {
+        return this.beers.filter((beer: Beer) => {
+            return beer.name 
+                && beer.name.match(new RegExp(this.pattern, 'i'));
+          }).length;
+    }
+
+    _beerSorter(a:Beer, b:Beer) {  
+        var invert = 1;
+        if (this.descendingSort) invert = -1;
+        if ( a[this.criterion.name] === b[this.criterion.name] ) return 0;
+        if ( a[this.criterion.name] < b[this.criterion.name] ) return -1*invert;
+        if ( a[this.criterion.name] > b[this.criterion.name] ) return 1*invert;      
+    }
+
+    _sortingChanged(evt: Event) {
+        this.criterion = { 
+            name: (evt.target as HTMLSelectElement).selectedOptions[0].value,
+            label: (evt.target as HTMLSelectElement).selectedOptions[0].text,
+        }
+    }
+
+    _descendingChange(evt: Event) {
+        this.descendingSort = (evt.target as HTMLInputElement).checked;
+    }
+
+    render() {
+        return <div class="container">
+            <div class="row">
+                <div class="col-md-3">
+                    <div class="form-group">
+                        <label 
+                            htmlFor="search">
+                            Search
+                        </label>
+                        <input 
+                            type="text" 
+                            class="form-control" 
+                            id="search"  
+                            onInput={(evt) => this.doFilter(evt)}  
+                            placeholder="Enter search"></input>
+                    </div>
+                    <div>Current search: {this.pattern}</div>                    
+                    <label 
+                        htmlFor="sort">
+                    Sort by
+                    </label>
+                    <select 
+                        id="sort" 
+                        class="form-select"
+                        onChange={(evt) => this._sortingChanged(evt)}>
+                        { sortingCriteria.map((item) => <option value={item.name}> {item.label}</option>) }
+                    </select>
+                    <label htmlFor="descending">Descending sort</label>
+                    <input 
+                        id="descending" 
+                        type="checkbox" 
+                        class="form-check-input"
+                        onChange={(evt) => this._descendingChange(evt)}></input>
+                </div>
+                <div class="col-md-9">
+                    <ul class="container beers">
+                        {this.beers
+                        .filter( (beer: Beer) => {
+                            return beer.name 
+                                && beer.name.match(new RegExp(this.pattern, 'i'));
+                          })
+                        .sort((a,b) => this._beerSorter(a,b))
+                        .map((beer: Beer) => {
+                            return <li>
+                                <beer-list-item 
+                                    name={beer.name} 
+                                    description={beer.description}
+                                    alcohol={beer.alcohol}></beer-list-item>
+                            </li>;
+                        })}
+                    </ul>
+                    <div class="container">
+                        <div>Number of beers in list: {this._currentBeers()}</div>
+                    </div>
+                </div>  
+          </div>        
+        </div>;
+    }
+}
+
